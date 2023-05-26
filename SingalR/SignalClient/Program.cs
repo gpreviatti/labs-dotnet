47c54df9db621f9b403e0a35c141bf29773307f1
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2023-05-26 09:07:44 -0300

    add signalR poc

---
+++ SingalR/SignalClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var uri = "http://localhost:5000/chat";
+
+await using var connection = new HubConnectionBuilder().WithUrl(uri).Build();
+
+await connection.StartAsync();
+
+await foreach (var date in connection.StreamAsync<DateTime>("Streaming"))
+    Console.WriteLine(date);
