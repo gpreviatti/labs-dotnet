dbe9976fb635f81df1b233da8617e2a93946b4b6
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2023-09-05 07:44:45 -0300

    first commit

---
+++ DesignPatternsDotnet/Commons/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace Commons.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public int Total { get; set; }
+    public float Value { get; set; }
+}
---
+++ DesignPatternsDotnet/Commons/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace Commons.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public float Value { get; set; }
+
+    IEnumerable<Item> OrderItems { get; set; }
+}
