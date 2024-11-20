4ba2213a09b703e2f0fecd1bc2c9f6b9d835985e
Giovanni B. Previatti <giovanni.previatti@outlook.com>
2024-11-20 11:29:05 -0300

    add working with tasks project

---
+++ WorkingWithTasks/Program.cs
@@ -0,0 +1,13 @@
+﻿static async Task<int> Sum()
+{
+    await Task.Delay(5000);
+
+    return 10;
+}
+
+var number1 = Sum();
+var number2 = Sum();
+
+await Task.WhenAll(number1, number2);
+
+Console.WriteLine(number1.Result + number2.Result);
